4009acc2da3b02c6c02fbcb1243be8565d3e8866
Maksym Korzh <freesoft.for.people@gmail.com>
2019-05-02 13:25:30 +0300

    documented 'FreshProxyList.py'

--- src/FreshProxyList.py
+++ src/FreshProxyList.py
@@ -1,15 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
+# variable to store working proxies
 proxyList = []
 
+# make reguest to 'https://free-proxy-list.net' to scrape proxies
 response = requests.get('https://free-proxy-list.net')
+
+# parse response with beautiful soup
 bs = BeautifulSoup(response.text, 'lxml')
 
-table = bs.find('table')
-rows = table.find_all('tr')
+table = bs.find('table')        # find proxy table element
+rows = table.find_all('tr')     # fidn list of all rows in table
 
 def init_proxy_list(*args):
+    '''
+    Function defines each single row in a table with corresponding values,
+    extracts only those proxies that support 'https' and typed as 'anonymous'
+    or 'elite proxy', tests each proxy via connecting to 'https://www.httpbin.org/ip'
+    and if response returns the corresponding IP then the proxy is appended to list.
+    Function can take optional 'limit' argument of integer type to define the number
+    of proxies to be scraped and stored.
+    '''
     for row in rows:
         ip = row.contents[0].text
         port = row.contents[1].text
@@ -46,6 +58,10 @@ def init_proxy_list(*args):
                 break
 
 def write_to_file():
+    '''
+    Writes 'proxyList' elements to file 'fresh_proxies.txt' in case
+    if at least one entry is available.
+    '''
     if len(proxyList) == 0:
         print('Please call "init_proxy_list([limit])" first')
     with open('fresh_proxies.txt', 'w') as f:
